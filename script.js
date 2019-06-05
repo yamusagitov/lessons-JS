1600f4acfccc295278c170f454aa77de9b6299b6
IURII <yamusagitov@gmail.com>
2019-06-05 15:46:13 +0500

    Lessons JS

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+let a = 3;
+console.log(a);
+
+var leftForderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+let arr = ['plum.png', 'juce.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+console.log(pi);
+
+//alert("Hello, World!");
+
+
+//let answer = confirm("Are you here?");
+//console.log(answer);
+
+//let answer = prompt("Есть ли вам 18?", "");
+//console.log(answer);
+
+let answer = prompt("Есть ли вам 18?", "");
+console.log(typeof(answer));                        //тип данных
+
+
+let incr = 10,
+decr = 10;
+
+console.log(++incr);
+console.log(--decr);
